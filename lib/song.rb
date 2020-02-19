8da9358900a19cd39905b162b8b4909e0d56b759
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-19 20:20:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,7 @@
 class Song 
-  attr_accessor :name, :artists, :genre 
+  attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  @@artists = []
-  @@genres = []
   
   def initialize (name, artist, genre)
     
