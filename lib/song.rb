4afe4124665f0fdb821c69b5966f1847a11b0dd4
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-19 20:54:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,11 +26,23 @@ class Song
     @@artists.uniq
   end
   
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
   
   
   def self.genres
     @@genres.uniq
   end
+
 
 
 end
